ef7ba80be0ff91d5e356ecac232aad5daebf59a0
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2023-08-09 22:19:50 -0300

    Finished extracting, doesn't work

--- main.cpp
+++ main.cpp
@@ -23,6 +23,12 @@ void drawRect(cv::Mat &img, cv::Point2f points[]){
             cv::line(img, points[i], points[(i+1)%4], cv::Scalar(0, 0, 255));
 }
 
+void joinStrs(std::string &res, const std::vector<std::string> &strs, const int uBound, const std::string &sep){
+    res = "";
+    for(int i = 0; i < uBound; ++i)
+        res += strs[i] + sep;
+}
+
 xml::xml_document<> *getXmlDoc(const std::string &fname){
     std::ifstream fnameStream(fname);
     std::vector<char> fContent( (std::istreambuf_iterator<char>(fnameStream)), std::istreambuf_iterator<char>());
@@ -54,58 +60,67 @@ void cropRotatedRect(const cv::Mat &img, cv::Mat &croppedImg, const cv::RotatedR
 }
 
 int main(){
-    const std::string DS_PATH = "../PKLot/PKLot/";
-    const std::string DS_SEG_PATH = "../PKLot/MyPKLotSeg/";
-    const std::string cur_dir = "PUCPR/Cloudy/2012-09-12/";
-    const std::string file_pref = "2012-09-12_06_10_30";
 
-    const std::string img_fname = DS_PATH + cur_dir + file_pref + ".jpg";
-    const std::string xml_fname = DS_PATH + cur_dir + file_pref + ".xml";
+    const std::string DS_PATH = "../PKLot/PKLot/UFPR05";
+    const std::string DS_SEG_PATH = "../PKLot/MyPKLotSeg/UFPR05";
 
-    fs::create_directories(DS_SEG_PATH + cur_dir + "Empty/");
-    fs::create_directories(DS_SEG_PATH + cur_dir + "Occupied/");
+    for (const auto &dirEnt : fs::recursive_directory_iterator(DS_PATH)){
+        if (fs::is_directory(dirEnt) || dirEnt.path().extension() != ".jpg") continue;
 
-    // read image
-    cv::Mat cv_img = cv::imread(img_fname);
+        std::cout << "Processing " << dirEnt << " ..." << std::endl;
 
-    // get parking node
+        std::string file_pref{dirEnt.path().stem()};
+        std::string cur_dir{dirEnt.path().parent_path()};
+        cur_dir.erase(0, DS_PATH.size()); cur_dir += "/";
 
-    xml::xml_document<> *doc = getXmlDoc(xml_fname);
-    xml::xml_node<> *parking = doc->first_node("parking");
-    xml::xml_node<> *space = parking->first_node();
-    for(; space; space = space->next_sibling()){
-        xml::xml_node<> *rect_node = space->first_node("rotatedRect");
-        
-        int id = atoi(space->first_attribute("id")->value());
-        int occ = atoi(space->first_attribute("occupied")->value());
+        std::string empty_dir = DS_SEG_PATH + cur_dir + "Empty/";
+        std::string occup_dir = DS_SEG_PATH + cur_dir + "Occupied/";
 
-        cv::RotatedRect rotRect;
+        fs::create_directories(empty_dir);
+        fs::create_directories(occup_dir);
 
-        // get xml data
-        xml::xml_node<> *center_node = rect_node->first_node("center");
-        rotRect.center.x = atof(center_node->first_attribute("x")->value());
-        rotRect.center.y = atof(center_node->first_attribute("y")->value());
+        const std::string img_input = DS_PATH + cur_dir + file_pref + ".jpg";
+        const std::string xml_input = DS_PATH + cur_dir + file_pref + ".xml";
 
-        xml::xml_node<> *size_node = rect_node->first_node("size");
-        rotRect.size.width  = atof(size_node->first_attribute("w")->value());
-        rotRect.size.height = atof(size_node->first_attribute("h")->value());
+        cv::Mat cv_img = cv::imread(img_input);
 
-        xml::xml_node<> *angle_node = rect_node->first_node("angle");
-        rotRect.angle = atof(angle_node->first_attribute("d")->value());
+        xml::xml_document<> *doc = getXmlDoc(xml_input);
+        xml::xml_node<> *parking = doc->first_node("parking");
+        xml::xml_node<> *space = parking->first_node();
 
-        // Crop rectangle
-        cv::Mat cropped_img;
-        cropRotatedRect(cv_img, cropped_img, rotRect);
-        
+        for(; space; space = space->next_sibling()){
+            xml::xml_node<> *rect_node = space->first_node("rotatedRect");
+            
+            int id = atoi(space->first_attribute("id")->value());
+            int occ = atoi(space->first_attribute("occupied")->value());
 
-        std::string fPath = DS_SEG_PATH + cur_dir;
-        fPath += occ ? "Occupied/" : "Empty/";
-        fPath += file_pref + "#" + std::format("{:03}",id) + ".jpg";
+            cv::RotatedRect rotRect;
 
-        cv::imwrite(fPath, cropped_img);
+            // get xml data
+            xml::xml_node<> *center_node = rect_node->first_node("center");
+            rotRect.center.x = atof(center_node->first_attribute("x")->value());
+            rotRect.center.y = atof(center_node->first_attribute("y")->value());
+
+            xml::xml_node<> *size_node = rect_node->first_node("size");
+            rotRect.size.width  = atof(size_node->first_attribute("w")->value());
+            rotRect.size.height = atof(size_node->first_attribute("h")->value());
+
+            xml::xml_node<> *angle_node = rect_node->first_node("angle");
+            rotRect.angle = atof(angle_node->first_attribute("d")->value());
+
+            // Crop rectangle
+            cv::Mat cropped_img;
+            cropRotatedRect(cv_img, cropped_img, rotRect);
+            
+            std::string fPath = DS_SEG_PATH + cur_dir;
+            fPath += occ ? "Occupied/" : "Empty/";
+            fPath += file_pref + "#" + std::format("{:03}",id) + ".jpg";
+
+            cv::imwrite(fPath, cropped_img);
+        }
+
+        delete doc;
     }
-
-    delete doc;
 
     return 0;
 }
